6ab68c35e95c93827d9895fb0140b997b8d47ff1
Soroush Amel <soroushzendedel1@gmail.com>
2024-12-14 10:26:05 +0330

    updated endpoint

--- script.js
+++ script.js
@@ -142,7 +142,7 @@ function updateMilestones(elapsedTime) {
 async function sendMilestoneUpdate(telegramId, milestone, milestoneTime) {
   try {
     // Replace with your publicly accessible HTTPS URL (e.g. ngrok URL)
-    const response = await fetch("https://96d3-188-132-129-196.ngrok-free.app/update-milestone", {
+    const response = await fetch("https://da1a-188-132-129-196.ngrok-free.app/update-milestone", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
